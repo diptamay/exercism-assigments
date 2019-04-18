82c8d03bd5f9a6b926d4696d07d0adfbfb56dc1e
diptamay.sanyal <diptamay.sanyal@adp.com>
2019-04-18 14:07:23 -0400

    flatten done

---
+++ go/largest-series-product/largest_series_product.go
@@ -0,0 +1,7 @@
+// Package lsproduct contains LargestSeriesProduct
+package lsproduct
+
+// LargestSeriesProduct given a string of digits, calculate the largest product for a contiguous substring of digits of length n.
+func LargestSeriesProduct(s string, i int) (int, error) {
+	return 0, nil
+}

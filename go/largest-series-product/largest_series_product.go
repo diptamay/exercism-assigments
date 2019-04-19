e9d80432ae868a9afaa6f4387ed6c9252ce1b728
Diptamay Sanyal <diptamay@Diptamays-MacBook-Pro.local>
2019-04-18 22:19:33 -0400

    largest sp

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,7 +1,32 @@
 // Package lsproduct contains LargestSeriesProduct
 package lsproduct
 
+import (
+	"errors"
+)
+
 // LargestSeriesProduct given a string of digits, calculate the largest product for a contiguous substring of digits of length n.
-func LargestSeriesProduct(s string, i int) (int, error) {
-	return 0, nil
+func LargestSeriesProduct(str string, span int) (int, error) {
+	length := len(str)
+	if length < span {
+		return -1, errors.New("span must be smaller than string length")
+	}
+	if span < 0 {
+		return -1, errors.New("span must be greater than zero")
+	}
+	product := -1
+	for i := 0; i <= length-span; i++ {
+		temp := 1
+		for j := 0; j < span; j++ {
+			v := int(str[i+j] - '0')
+			if v > 9 {
+				return -1, errors.New("digits input must only contain digits")
+			}
+			temp = temp * v
+		}
+		if temp > product {
+			product = temp
+		}
+	}
+	return product, nil
 }
